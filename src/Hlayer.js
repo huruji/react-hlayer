c043cd088c2b53206bdab377219b17fe347fdfce
huruji <594613537@qq.com>
2017-05-22 17:15:47 +0800

    compelete the config::rocket

--- src/Hlayer.js
+++ src/Hlayer.js
@@ -9,57 +9,49 @@ import Photo from './Photo';
 import Tips from './Tips';
 import Music from './Music';
 
-const defaultConfig = {
-  type:'msg',
-  mainBg:'#169FE6',
-  mainColor: '#fff',
-  contentBg:'#fff',
-  contentColor:'#000',
-  title: '信息',
-  text: '提示信息',
-  width: '',
-  height: '',
-  btns:'',
-  confirmBtn: true,
-  confirmCb: false,
-  cancelBtn: false,
-  cancelCb: false,
-  animateType: 1,
-  resize:true,
-  position: 0,
-  shadow:true,
-  time: false,
-  loadingType:1,
-  closeBtn:false,
-  url:false,
-  formType:1,
-  move:true,
-  photos:false,
-  closeType:1,
-  tipsPosition:'right',
-  tipsCon:'',
-  autoPlay:false,
-  playTime:5000,
-  allowEmpty:true,
-};
+import {defaultConfig, msgConfig, alertConfig, loadingConfig, iframeConfig, photoConfig, promptConfig, tipsConfig, musicConfig} from './config';
 
 class Hlayer extends Component{
   constructor(){
     super();
   }
   render(){
-    const config = {...defaultConfig,...this.props};
+    let config = {};
     let shadow = null;
     let content = null;
     if(config.shadow){
       shadow = <div className="hlayer-shadow"></div>
     }
-
+    switch(this.props.type){
+      case 'msg':
+        config = {...defaultConfig, ...msgConfig.change, ...this.props, ...msgConfig.noChange};
+        return <Msg {...config}/>;
+      case 'alert':
+        config = {...defaultConfig, ...alertConfig.change, ...this.props, ...alertConfig.noChange};
+        return <Alert {...config}/>;
+      case 'loading':
+        config = {...defaultConfig, ...loadingConfig.change, ...this.props, ...loadingConfig.noChange};
+        return <Loading {...config}/>;
+      case 'iframe':
+        config = {...defaultConfig, ...iframeConfig.change, ...this.props, ...iframeConfig.noChange};
+        return <Iframe {...config}/>;
+      case 'prompt':
+        config = {...defaultConfig, ...promptConfig.change, ...this.props, ...promptConfig.noChange};
+        return <Prompt {...config}/>;
+      case 'photo':
+        config = {...defaultConfig, ...photoConfig.change, ...this.props, ...photoConfig.noChange};
+        return <Photo {...config}/>;
+      case 'tips':
+        config = {...defaultConfig, ...tipsConfig.change, ...this.props, ...tipsConfig.noChange};
+        return <Tips {...config}/>;
+      case 'music':
+        config = {...defaultConfig, ...musicConfig.change, ...this.props, ...musicConfig.noChange};
+        return <Music{...config}/>
+    }
     return(
         <div className="hlayer">
           {shadow}
           <div className={`hlayer-content hlayer-${config.type} hlayer-animate${config.animateType}`} style={{width:config.width,height:config.height}}>
-
           </div>
         </div>
     )
