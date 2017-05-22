dad02b3a82b4528278db3748e3ec4f46f6ae7756
huruji <594613537@qq.com>
2017-05-22 16:50:25 +0800

    make the base items::rocket

---
+++ src/Alert.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+class Alert extends Component{
+  render(){
+    return(
+        <div>Alert</div>
+    )
+  }
+}
+
+export default Alert;
--- src/Hlayer.js
+++ src/Hlayer.js
@@ -1,12 +1,67 @@
 import React,{Component} from 'react';
 
+import Msg from './Msg';
+import Alert from './Alert';
+import Loading from './Loading';
+import Iframe from './Iframe';
+import Prompt from './Prompt';
+import Photo from './Photo';
+import Tips from './Tips';
+import Music from './Music';
+
+const defaultConfig = {
+  type:'msg',
+  mainBg:'#169FE6',
+  mainColor: '#fff',
+  contentBg:'#fff',
+  contentColor:'#000',
+  title: '信息',
+  text: '提示信息',
+  width: '',
+  height: '',
+  btns:'',
+  confirmBtn: true,
+  confirmCb: false,
+  cancelBtn: false,
+  cancelCb: false,
+  animateType: 1,
+  resize:true,
+  position: 0,
+  shadow:true,
+  time: false,
+  loadingType:1,
+  closeBtn:false,
+  url:false,
+  formType:1,
+  move:true,
+  photos:false,
+  closeType:1,
+  tipsPosition:'right',
+  tipsCon:'',
+  autoPlay:false,
+  playTime:5000,
+  allowEmpty:true,
+};
+
 class Hlayer extends Component{
   constructor(){
     super();
   }
   render(){
+    const config = {...defaultConfig,...this.props};
+    let shadow = null;
+    let content = null;
+    if(config.shadow){
+      shadow = <div className="hlayer-shadow"></div>
+    }
+
     return(
-        <div className="hlayer"></div>
+        <div className="hlayer">
+          {shadow}
+          <div className={`hlayer-content hlayer-${config.type} hlayer-animate${config.animateType}`} style={{width:config.width,height:config.height}}>
+
+          </div>
+        </div>
     )
   }
 }
