a9524ee0e7f7659912bacb338087b878767ceff1
huruji <594613537@qq.com>
2017-05-22 16:18:41 +0800

    start this project::rocket

---
+++ src/Hlayer.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+class Hlayer extends Component{
+  constructor(){
+    super();
+  }
+  render(){
+    return(
+        <div className="hlayer"></div>
+    )
+  }
+}
