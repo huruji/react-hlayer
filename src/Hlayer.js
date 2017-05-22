04bf2ffc85bcfd91d1ad75cb91dc890a5f15831f
huruji <594613537@qq.com>
2017-05-22 17:17:35 +0800

    compelete the config::rocket

--- src/Hlayer.js
+++ src/Hlayer.js
@@ -25,35 +25,38 @@ class Hlayer extends Component{
     switch(this.props.type){
       case 'msg':
         config = {...defaultConfig, ...msgConfig.change, ...this.props, ...msgConfig.noChange};
-        return <Msg {...config}/>;
+        return content = <Msg {...config}/>;
       case 'alert':
         config = {...defaultConfig, ...alertConfig.change, ...this.props, ...alertConfig.noChange};
-        return <Alert {...config}/>;
+        return content = <Alert {...config}/>;
       case 'loading':
         config = {...defaultConfig, ...loadingConfig.change, ...this.props, ...loadingConfig.noChange};
-        return <Loading {...config}/>;
+        return content = <Loading {...config}/>;
       case 'iframe':
         config = {...defaultConfig, ...iframeConfig.change, ...this.props, ...iframeConfig.noChange};
-        return <Iframe {...config}/>;
+        return content = <Iframe {...config}/>;
       case 'prompt':
         config = {...defaultConfig, ...promptConfig.change, ...this.props, ...promptConfig.noChange};
-        return <Prompt {...config}/>;
+        return content = <Prompt {...config}/>;
       case 'photo':
         config = {...defaultConfig, ...photoConfig.change, ...this.props, ...photoConfig.noChange};
-        return <Photo {...config}/>;
+        return content = <Photo {...config}/>;
       case 'tips':
         config = {...defaultConfig, ...tipsConfig.change, ...this.props, ...tipsConfig.noChange};
-        return <Tips {...config}/>;
+        return content = <Tips {...config}/>;
       case 'music':
         config = {...defaultConfig, ...musicConfig.change, ...this.props, ...musicConfig.noChange};
-        return <Music{...config}/>
+        return content = <Music{...config}/>
     }
     return(
         <div className="hlayer">
           {shadow}
           <div className={`hlayer-content hlayer-${config.type} hlayer-animate${config.animateType}`} style={{width:config.width,height:config.height}}>
+            {content}
           </div>
         </div>
     )
   }
 }
+
+export default Hlayer;
