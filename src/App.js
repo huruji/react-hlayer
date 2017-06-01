36e3d4f081907b866d46c2127f03d8e9e6e5920a
huruji <594613537@qq.com>
2017-06-02 00:51:43 +0800

    add iframe type:rocket:

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Hlayer from './Hlayer';
 class App extends Component {
   constructor(){
     super();
-    this.state = {msgshow:false,alertshow:false, loadingshow:false};
+    this.state = {msgshow:false,alertshow:false, loadingshow:false, iframeshow: false};
     this.handleMsgShow = this.handleMsgShow.bind(this);
     this.handleAlertShow = this.handleAlertShow.bind(this);
     this.handleLoadingShow = this.handleLoadingShow.bind(this);
+    this.handleIframeShow = this.handleIframeShow.bind(this);
   }
   handleMsgShow(show){
     this.setState({msgshow: show});
@@ -19,9 +20,12 @@ class App extends Component {
   handleLoadingShow(show){
     this.setState({loadingshow: show});
   }
+  handleIframeShow(show) {
+    this.setState({iframeshow: show})
+  }
   render() {
     const inputStyle = {height: '50px', padding: '10px', background: '#fff', minWidth: '100px', cursor: 'pointer', fontSize: '14px', margin: '20px 20px 0 0', borderRadius: '2px', border: '1px solid #aaa',};
-    let lay = null, alertlay=null, loadinglay;
+    let lay = null, alertlay=null, loadinglay = null, iframelay = null;
     if(this.state.msgshow){
       lay=<Hlayer type="msg" handleShow={this.handleMsgShow} config = {{contentBg: '#169fe6' ,contentColor:"#fff" ,animateType:4, position: 3, shadow: false, text:'追风少年',   time: 30000, icon:4,}}/>
     }
@@ -31,14 +35,19 @@ class App extends Component {
     if(this.state.loadingshow){
       loadinglay=<Hlayer type="loading" handleShow={this.handleLoadingShow} config = {{animateType:4, position: 0, shadow: false,    time: 4000, loadingType: 4}}/>
     }
+    if(this.state.iframeshow){
+      iframelay=<Hlayer type="iframe" handleShow={this.handleIframeShow} config = {{animateType:4, position: 0, shadow: false,time: 400000,src: 'http://hlayer.huruji3.com/'}}/>
+    }
     return (
       <div className="App">
         <input style={inputStyle} value="Msg" type="button" onClick={()=>{this.setState((prevState) => ({msgshow: true}))}}/>
         {lay}
         {alertlay}
         {loadinglay}
+        {iframelay}
         <input style={inputStyle} type="button" value="alert"  onClick={()=>{this.setState((prevState) => ({alertshow: true}))}}/>
         <input style={inputStyle} type="button" value="loading"  onClick={()=>{this.setState((prevState) => ({loadingshow: true}))}}/>
+        <input style={inputStyle} type="button" value="iframe"  onClick={()=>{this.setState((prevState) => ({iframeshow: true}))}}/>
       </div>
     );
   }
--- src/Iframe.js
+++ src/Iframe.js
@@ -2,8 +2,17 @@ import React,{Component} from 'react';
 
 class Iframe extends Component{
   render(){
+    let titleStyle, contentStyle;
+    titleStyle = {background: this.props.mainBg,color:this.props.mainColor};
+    contentStyle = {height: parseInt(this.props.height) - 52 + 'px'};
     return(
-        <div>Iframe</div>
+        <div>
+          <div className="hlayer-content-title hlayer-alert" style={titleStyle}>{this.props.title}</div>
+          <div className="hlayer-content-main hlayer-alert-content" >
+            <iframe className="hlayer-content-iframe" src={this.props.src} style={contentStyle}></iframe>
+          </div>
+          <div className="hlayer-close hlayer-iframeclose hlayer-close1" onClick={() => this.props.close()}></div>
+        </div>
     )
   }
 }
